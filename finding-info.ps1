48a083f1c33aa8922a744d0e97fb8073d76ae970
Luiz Oberto <lom.raiol@gmail.com>
2025-04-03 14:35:52 -0300

    update finding-info.ps1

--- finding-info.ps1
+++ finding-info.ps1
@@ -1,3 +1,6 @@
+Add-Type -AssemblyName System.Web
+
+
 $ip = "(YOUR_URL)"
 
 $response = & "C:\Windows\System32\curl.exe" -k $ip | Out-String
@@ -11,7 +14,13 @@ $matches = [regex]::Matches($response, $regex)
 # Verificar o que encontrou
 if ($matches.Count -gt 0) {
     foreach ($match in $matches) {
-        Write-Output "Valor encontrado: $($match.Groups[1].Value)"
+        $conteudo_decodificado = [System.Web.HttpUtility]::HtmlDecode($match.Groups[1].Value)
+        if ($conteudo_decodificado -ne "Número total de páginas de impressão em 2 faces :") {
+            Write-Host $conteudo_decodificado
+        } else {
+            break
+        }
+        
     }
 } else {
     Write-Output "Não foi possível encontrar a informação."
